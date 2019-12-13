e50a51bc56be97de450ccd3178713f55efd05974
Khalil <mk.bensalah@gmail.com>
2019-12-13 13:09:19 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {MainComponentComponent} from '../app/components/main-component/main-component.component';
+import {TicketTypeComponent} from '../app/components/ticket-type/ticket-type.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponentComponent},
+  {path: 'TicketType', component: TicketTypeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { TicketTypeComponent } from './components/ticket-type/ticket-type.component';
+import { MainComponentComponent } from './components/main-component/main-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TicketTypeComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
